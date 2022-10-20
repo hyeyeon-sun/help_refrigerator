70887136c6a5f1977bc95fdfcebebd9d9b9962c1
HyeyeonLee <dlgpdus0416@naver.comm>
2022-10-20 10:17:48 +0900

    서버 초기 세팅

---
+++ help_refrigerator/src/main/java/com/example/help_refrigerator/HelpRefrigeratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.help_refrigerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpRefrigeratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpRefrigeratorApplication.class, args);
+	}
+
+}
---
+++ help_refrigerator/src/test/java/com/example/help_refrigerator/HelpRefrigeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.help_refrigerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpRefrigeratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
