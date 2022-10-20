d6606d513635187a612d62e46b44b9037bdfcf77
HyeyeonLee <dlgpdus0416@naver.comm>
2022-10-20 21:16:02 +0900

    domain 명시

--- help_refrigerator/src/main/java/com/example/help_refrigerator/feature/domain/food_category.java
+++ help_refrigerator/src/main/java/com/example/help_refrigerator/feature/domain/food_category.java
@@ -16,4 +16,21 @@ public class food_category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer foodId;
+
+    @Column(length = 1000, nullable = false)
+    private String food_category_id;
+
+    @Column(length = 1000, nullable = false)
+    private String name;
+
+    @Column(length = 1000, nullable = false)
+    private String expiration_date;
+
+    @Column(length = 1000, nullable = false)
+    private String image_url;
+
+    @Column(length = 1000, nullable = false)
+    private String created_at;
+
+
 }
